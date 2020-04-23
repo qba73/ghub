d1d95c25cb5326acd5b81bec8cef0eaca8df1511
Jakub Jarosz <jakub@jarosz.dev>
2020-04-23 21:20:53 +0100

    first example

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	example_basic "github.com/qba73/ghub/src/basic"
+)
 
 func main() {
-	fmt.Println("Profobuf test")
+	doBasic()
+
+}
+
+func doBasic() {
+	bm := example_basic.BasicMessage{
+		Id:         1234,
+		IsSimple:   true,
+		Name:       "First basic message",
+		SampleList: []int32{1, 2, 3, 4},
+	}
+
+	fmt.Println(bm)
 }
